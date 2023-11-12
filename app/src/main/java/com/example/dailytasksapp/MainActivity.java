bff88d35110720c155f38e6c11133857379dcf53
Jeferson X <jefersonx0140@gmail.com>
2023-11-12 13:23:13 +0530

    DatabaseHelper

--- app/src/main/java/com/example/dailytasksapp/MainActivity.java
+++ app/src/main/java/com/example/dailytasksapp/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.dailytasksapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.dailytasksapp.Utils.DatabaseHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private FloatingActionButton fab;
+    private DatabaseHelper myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+        fab = findViewById(R.id.fab);
+        myDB = new DatabaseHelper(MainActivity.this);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
